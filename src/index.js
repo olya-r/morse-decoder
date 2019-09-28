08dca26c253767e9bf812371cbb14d2ce2e115b7
olya <olya_r@tut.by>
2019-09-28 19:36:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,7 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const MORSE_TABLE2 = {
+    '**********': ' ',
+};
+
+for (key in MORSE_TABLE) {
+    MORSE_TABLE2[encode(key)] = MORSE_TABLE[key];
+}
+
+function encode(key) {
+    return key
+            .split('')
+            .map(ch => {
+                switch (ch) {
+                    case '.': return '10';
+                    case '-': return '11';
+                    default: throw(new Error(`Bad data in MORSE_TABLE key ${key}`))
+                }
+            })
+            .join('')
+            .padStart(10, '0');
+}
+
 function decode(expr) {
+    const result = [];
+    for (i = 0; i < expr.length; i += 10) {
+        result.push(MORSE_TABLE2[expr.slice(i, i + 10)]);
+    }
+    return result.join('');
     // write your solution here
 }
 
